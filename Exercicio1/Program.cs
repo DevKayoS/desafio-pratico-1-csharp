6dd0c1bdfe7f2f0f675a02ffc3fef7cfa04d6542
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-08-30 14:27:47 -0300

    Fazendo os exercicios 1 e 2 do desafio pratico

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Desafio1;
+
+class Program
+{
+    static void Main()
+    {
+        string name = Console.ReadLine();
+
+        Console.WriteLine($"Olá, {name}! Seja muito bem-vindo!");
+    }
+}
+
