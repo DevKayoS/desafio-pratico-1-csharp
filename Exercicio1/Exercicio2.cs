165f393ff988bc042ec5c54d79864cb60ee1d012
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-08-30 14:31:11 -0300

    fazerndo 2

---
+++ Exercicio1/Exercicio2.cs
@@ -0,0 +1,12 @@
+namespace Desafio1;
+
+public class Exercicio2
+{
+    static void Exercicio2Bom()
+    {
+        string firstName = Console.ReadLine();
+        string lastName = Console.ReadLine();
+        string fullName = $"{firstName} {lastName}";
+        Console.WriteLine(fullName);
+    }
+}
